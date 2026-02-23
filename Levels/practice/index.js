67b77415cf927385ba69dc980809af2772961311
Devansh Tyagi <86757420+DevanshTyagiii@users.noreply.github.com>
2026-02-23 20:33:29 +0530

    practice

--- Levels/practice/index.js
+++ Levels/practice/index.js
@@ -1,2 +1,14 @@
 let cartQty = 5;
 document.querySelector('#cart-summary').innerText = `Your bag has ${cartQty} items`;
+
+let pi = 3.14159;
+
+let bagValue = Math.round(Math.random() * 1000);
+
+document.querySelector('#bagValue').innerText = `Your bag is worth $${bagValue}`;
+
+let name = "Devansh";
+
+document.querySelector('#name').innerText = `My name is ${name}`;
+
+document.querySelector('#pi').innerText = `The value of pi is approximately ${pi}`;
